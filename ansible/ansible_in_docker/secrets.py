6f77fc5a8c4c7effa18f5778a3da960d4fadba27
Ildar Saidashev <isaidashev@gmail.com>
2018-03-30 14:55:43 +0300

    Server for branch. Dynamic Inventory Ansible. Fix setting apache-libcloud

--- ansible/ansible_in_docker/secrets.py
+++ ansible/ansible_in_docker/secrets.py
@@ -1,3 +1,2 @@
 GCE_PARAMS = ('54306751049-compute@developer.gserviceaccount.com', '~/.PRIV.pem')
-GCE_KEYWORD_PARAMS = {'project': 'testing-gitlab-198014'}
-GCE_KEYWORD_PARAMS = {'datacenter': 'europe-west1-d'}
+GCE_KEYWORD_PARAMS = {'project': 'testing-gitlab-198014', 'datacenter': 'europe-west1-d'}
