4f28debb7982281f1c961b6b229a2bcdb23bf7f9
Ildar Saidashev <isaidashev@gmail.com>
2018-04-02 17:27:04 +0300

    Fix ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,6 @@ resource "google_compute_instance" "gitlab-ci" {
 
   metadata {
     #Добавление публичного ключа к инстансу
-    ssh-keys = "appuser:${var.public_key_path)}\ngitlab:${var.gitlab_public_key_path)}"
+    ssh-keys = "appuser:${var.public_key_path}\ngitlab:${var.gitlab_public_key_path}"
   }
 }
